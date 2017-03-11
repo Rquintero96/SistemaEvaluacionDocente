b14b474b2cc01cc89015d41ef7c00bac9ecbe4d2
Miguel Moretón <schulzmoreton@gmail.com>
2017-03-11 09:30:11 -0400

    Cambios para realizar la Auth
    Pruebas por ahora

--- src/server.js
+++ src/server.js
@@ -1,7 +1,28 @@
 var Hapi = require('hapi')
 var Vision = require('vision')
+
+//Inicializa el modulo de autentificacion
+var BasicAuth = require('hapi-auth-basic')
+    //y esta es la herramienta para las validaciones a usar
+    var Bcrypt = require('bcrypt')
+
+//Modulo para almacenar informacion a traves de la pagina
+var CookieAuth = require('hapi-auth-cookie')  
+
 var server = new Hapi.Server()
 
+///DEMOSTRACION///
+// hardcoded users object … just for illustration purposes
+var users = {  
+  future: {
+    id: '1',
+    username: 'future',
+    password: '$2a$04$YPy8WdAtWswed8b9MfKixebJkVUhEZxQCrExQaxzhcdR2xMmpSJiG'  // 'studio'
+  }
+}
+///FIN///
+
+//Conexion del servidor
 server.connection({
 	port: 8000
 });
@@ -9,6 +30,7 @@ server.connection({
 
 // register vision to your server instance
 server.register(Vision, function (err) {  
+    //Si ocurre algun error
   if (err) {
     console.log('Cannot register vision')
   }
@@ -24,6 +46,46 @@ server.register(Vision, function (err) {
   })
 })
 
+// Modulo para la verificacion de usuario
+server.register(BasicAuth, function (err) {
+
+     if (err) {
+        throw err
+        console.log('Menor hubo un error en el modulo de validacion')
+    }
+
+    // validation function used for hapi-auth-basic
+  var basicValidation  = function (request, username, password, callback) {
+    var user = users[ username ]
+
+    if (!user) {
+      return callback(null, false)
+    }
+
+    Bcrypt.compare(password, user.password, function (err, isValid) {
+      callback(err, isValid, { id: user.id, name: user.name })
+    })
+  }
+
+  //Estrategia a usar para la validacion
+  server.auth.strategy('simple', 'basic', { validateFunc: basicValidation })
+
+  //Ruta protegida
+  server.route({
+    method: 'GET',
+    path: '/prueba',
+    config: {
+    auth: 'simple',
+    handler: function (request, reply) {
+      reply('Si menor este mensaje solo lo pueden ver los usuarios autentificados plo plo');
+    }
+  }
+});
+})
+
+
+/*Modulo donde se definen las rutas de la aplicacion y permite ir de vista en vista. 
+Al agregar un view hay que agergar una nueva ruta en esta sesion*/
 server.route({
 
 	method: 'GET',
@@ -52,6 +114,7 @@ server.route({
         reply.view('registro-docente');
     }
 });
+
 
 server.route({
     method: 'GET',
@@ -116,8 +179,49 @@ server.register(require('inert'), function(err){
 	       	index: false
 	        }
     	}
+    });
 });
-});
+
+// Modulo de Cookies
+server.register(CookieAuth, function (err) {
+
+  server.auth.strategy('session', 'cookie', options) // your TODO: options -> there are required ones
+
+  // start your server after plugin registration
+  server.start(function (err) {
+    console.log('info', 'Server running at: ' + server.info.uri)
+  })
+})
+
+server.route({  
+  method: 'GET',
+  path: '/private-route',
+  config: {
+    auth: 'session',
+    handler: function (request, reply) {
+      reply('Yeah! This message is only available for authenticated users!')
+    }
+  }
+})
+
+server.route({  
+  method: 'POST',
+  path: '/login',
+  config: {
+    handler: function (request, reply) {
+      var username = request.payload.username
+      var password = request.payload.password
+
+      // check if user exists in DB
+      // compare passwords
+
+      // if everything went smooth, set the cookie with "user" specific data
+      request.cookieAuth.set(user);
+
+      reply('Wohoo, great to see you')
+    }
+  }
+})
 
 
 server.start(err => {
